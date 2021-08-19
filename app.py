80bdf3759b3ece95d4f9b34ef0e7fcdceb2d0f3e
bjabgma <bjorn.aaberg@gmail.com>
2021-08-19 16:39:30 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,28 +1,63 @@
-from flask import Flask
+from flask import Flask, request, render_template, make_response, send_file
 from datetime import datetime
-import re
+from pymongo import MongoClient
+import io
+import base64
+import pymongo
 
 app = Flask(__name__)
+
+client = pymongo.MongoClient(
+    "mongodb+srv://bjab:Emiafreabe69@cluster0.1pmw0.mongodb.net/track?retryWrites=true&w=majority"
+)
+
+app.db = client.trackDB
+Track = app.db["track"]
+
+Track.insert({'userId': 0})
+
+
+def getNextUserId():
+    currentId = Track.find({})[0]['userId']
+    nextId = currentId + 1
+    Track.update({}, {"$set": {"userId": nextId}})
+    return nextId
 
 
 @app.route("/")
 def home():
-    return "Hello, Bettan!"
+    print("Home")
+    return render_template("home.html")
 
 
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
 
-    if match_object:
-        clean_name = match_object.group(0)
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route('/static/pixel.gif')
+def returnPixel():
+    gif = 'R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
+    gif_str = base64.b64decode(gif)
+
+    if (request.cookies.get('userid')):
+        userid = request.cookies.get('userid')
     else:
-        clean_name = "Friend"
+        userid = getNextUserId()
 
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+    now = datetime.utcnow()
+    print(now, request.args.get('page'), userid)
+
+    response = make_response(
+        send_file(io.BytesIO(gif_str), mimetype='image/gif'), 200)
+    response.set_cookie('userid', str(userid), max_age=1296000)
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
